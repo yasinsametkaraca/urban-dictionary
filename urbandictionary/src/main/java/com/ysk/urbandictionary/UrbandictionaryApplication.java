cfe240d5d441aea3b072e43576fc393ac201f9fb
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2022-10-25 16:26:16 +0300

    project created

---
+++ urbandictionary/src/main/java/com/ysk/urbandictionary/UrbandictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.ysk.urbandictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbandictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbandictionaryApplication.class, args);
+	}
+
+}
---
+++ urbandictionary/src/test/java/com/ysk/urbandictionary/UrbandictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysk.urbandictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbandictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
